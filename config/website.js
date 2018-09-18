037f40b3c79c48904e4f417f0f2a480531bf3a5f
Navid Goalpure <Navid.goalpure@gmail.com>
2018-09-18 16:52:04 +0430

    refactor for eslint error

--- config/website.js
+++ config/website.js
@@ -4,14 +4,14 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'باشگاه کدنویسی جادوگران آینده', // Navigation and Site Title
-  siteTitleAlt: 'کدنویسی', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'باشگاه کدنویسی جادوگران آینده', // Alternative Site title for SEO
+  siteUrl: 'https://futureWizards.club', // Domain of your site. No trailing slash!
   siteLanguage: 'fa', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'معرفی و ثبت نام در دوره های آموزشی کدنویسی برای نوجوانان',
 
-  siteFBAppID: '123456789', // Facebook App ID
-  userTwitter: '@cara', // Twitter Username
+  // siteFBAppID: '123456789', // Facebook App ID
+  // userTwitter: '@cara', // Twitter Username
   ogSiteName: 'جادوگران آینده', // Facebook Site Name
   ogLanguage: 'fa', // Facebook Language
 
